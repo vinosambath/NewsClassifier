af6a1bc45960a3153675a65bab30f6d5842aa345
vinothkumar95 <vinosambath@gmail.com>
2016-12-12 01:05:43 +0530

    added exception handlers

--- Helpers/scrapper.py
+++ Helpers/scrapper.py
@@ -4,9 +4,12 @@ from BeautifulSoup 	import BeautifulSoup
 
 class scrapper:
 
-	def pageSoup(self, url):	
-		page = urllib2.urlopen(url)
-		return BeautifulSoup(page.read())
+	def pageSoup(self, url):
+		try:
+			page = urllib2.urlopen(url)
+			return BeautifulSoup(page.read())
+		except:
+			print("Unexpected error")
 
 #s = scrapper()
 #print(s.pageSoup("http://timesofindia.indiatimes.com"))
